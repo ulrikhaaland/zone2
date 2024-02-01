79418c0bc78987c96adaad74621bb59a24077a2d
Ulrik Håland <ulrik@netpower.no>
2024-02-01 16:32:09 +0100

    return url path

--- app/stores/auth.store.ts
+++ app/stores/auth.store.ts
@@ -183,7 +183,7 @@ export default class AuthStore {
     const actionCodeSettings = {
       // URL you want to redirect back to. The domain (www.example.com) for this
       // URL must be in the authorized domains list in the Firebase Console.
-      url: "https://zone2-liard.vercel.app/", // change this to your desired URL
+      url: `https://zone2-liard.vercel.app${path}`, // change this to your desired URL
       handleCodeInApp: true,
     };
 

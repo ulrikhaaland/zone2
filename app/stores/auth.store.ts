819bea0bcbbcdfe3f8a34c3f1c6b39aec295e3ea
Ulrik Håland <ulrik@netpower.no>
2024-02-12 15:56:49 +0100

    signin

--- app/stores/auth.store.ts
+++ app/stores/auth.store.ts
@@ -143,9 +143,7 @@ export default class AuthStore {
 
     const actionCodeSettings = {
       // Dynamically set URL based on whether the code is running on localhost or not
-      url: `${
-        isLocal ? "http://localhost:3000" : "https://zone2guide.com"
-      }${path}`,
+      url: `${isLocal ? "http://localhost:3000" : "https://zone2guide.com"}`,
       handleCodeInApp: true,
     };
 

a1a46c8564c964f6b554c65883832c7b694157f1
Ulrik Håland <ulrik@netpower.no>
2024-02-13 10:55:43 +0100

    asd

--- pages/api/stripe-webhook.ts
+++ pages/api/stripe-webhook.ts
@@ -10,7 +10,7 @@ import { handleOnGenerateGuide } from "@/pages/api/generate";
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
+      projectId: process.env.FIREBASE_PROJECT_ID!,
       privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"), // Replace escaped newlines back to actual newline characters
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       // The rest of the fields are optional for initializing Firebase Admin

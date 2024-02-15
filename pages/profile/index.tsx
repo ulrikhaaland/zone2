df421010083da614165883538f9760f29687edaa
Ulrik Håland <ulrikhaaland2@gmail.com>
2024-02-15 14:58:21 +0100

    asd

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -95,7 +95,7 @@ const UserProfile: NextPageWithLayout = () => {
         setGuideStatus(user.guideStatus);
       }
     }
-  }, [authStore, authStore.user, user, isFetching]); // Note: Added isFetching to the dependencies
+  }, [authStore, authStore.user]); // Note: Added isFetching to the dependencies
 
   const onGuideLoaded = () => {
     authStore

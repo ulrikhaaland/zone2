40ac4529aef97bf12a19a3ebb9e0be77a7cd2448
Ulrik Håland <ulrikhaaland2@gmail.com>
2024-05-16 11:08:48 +0200

    env vars

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,13 +17,13 @@ import { logPageView } from "@/app/analytics";
 const inter = Inter({ subsets: ["latin"] });
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCyS424Ct0bD6WXHuUcv2eI-lZjApJb5pU",
-  authDomain: "zone2program-a24ce.firebaseapp.com",
-  projectId: "zone2program-a24ce",
-  storageBucket: "zone2program-a24ce.appspot.com",
-  messagingSenderId: "505124953989",
-  appId: "1:505124953989:web:e7a050fa6f94ba3beb6eec",
-  measurementId: "G-LMBP764LCL",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
